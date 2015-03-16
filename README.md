0ee8ab2b087914281f9a818c8adb9e39c88de06a
postal2600 <postal2600@yahoo.com>
2015-03-16 23:47:32 +0200

    Implemented the initial working version of the package

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Selector to Tag
+[![Build Status](https://travis-ci.org/postal2600/selector-to-tag.svg?branch=master)](https://travis-ci.org/postal2600/selector-to-tag)
 
 Atom editor package that allows you to create HTML tag elements using CSS selectors in HTML files.
 
 More details comming soon ...
-
-[![Build Status](https://travis-ci.org/postal2600/selector-to-tag.svg?branch=master)](https://travis-ci.org/postal2600/selector-to-tag)
--- spec/selector-solver-spec.js
+++ spec/selector-solver-spec.js
@@ -1,11 +1,11 @@
 var SelectorSolver = require("../lib/selector-solver");
 
-describe("Detect HTML", function () {
+describe("HTML detector", function () {
     var solver;
 
     beforeEach(function () {
         solver = new SelectorSolver();
-    })
+    });
 
     it("should handle files with no extension", function () {
         expect(solver.isHTML("untitled")).toBe(false);
@@ -13,5 +13,33 @@ describe("Detect HTML", function () {
 
     it("should detect HTML files", function () {
         expect(solver.isHTML("test.HTML")).toBe(true);
-    })
+    });
 });
+
+describe("Tag solver", function () {
+    var solver;
+
+    beforeEach(function () {
+        solver = new SelectorSolver();
+    });
+
+    it("should solve tag alone", function () {
+        expect(solver.solveSelector("div").string).toBe('<div></div>');
+    });
+
+    it("should solve unknown tags", function () {
+        expect(solver.solveSelector("ceva").string).toBe('<ceva></ceva>');
+    });
+
+    it("should solve tag with id", function () {
+        expect(solver.solveSelector("div#mama").string).toBe('<div id="mama"></div>');
+    });
+
+    it("should solve tag with id and class", function () {
+        expect(solver.solveSelector("div#mama.tata").string).toBe('<div id="mama" class="tata"></div>');
+    });
+
+    it("should solve tag with id and multiple classes", function () {
+        expect(solver.solveSelector("div#mama.tata.sora").string).toBe('<div id="mama" class="tata sora"></div>');
+    });
+})
