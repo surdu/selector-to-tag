3b8f52a57b9a970e5998c76240d8709f6660c469
surdu <postal2600@gmail.com>
2016-03-01 18:17:20 +0200

    Finished rewriting the tests

--- spec/selector-solver-spec.js
+++ spec/selector-solver-spec.js
@@ -2,48 +2,34 @@ var SelectorSolver = require("../lib/selector-solver");
 var path = require('path');
 var fs = require('fs');
 
-function readExpected(file) {
-	return fs.readFileSync(path.join(__dirname, "./fixtures/after", file), "utf8");
-}
+describe("selector-to-tab", function () {
 
-function pressTab(editorView) {
-	var event = atom.keymaps.constructor.buildKeydownEvent("tab", {target: editorView});
-	console.log("event:", event);
-	atom.keymaps.handleKeyboardEvent(event);
-}
-
-describe("HTML detector", function () {
-	var solver;
-
-	beforeEach(function () {
-		solver = new SelectorSolver();
-	});
-
-	it("should handle files with no extension", function () {
-		expect(solver.isHTML("untitled")).toBe(false);
-	});
-
-	it("should detect HTML files", function () {
-		expect(solver.isHTML("test.HTML")).toBe(true);
-	});
-});
-
-describe("Tag solver2", function () {
 	var editor;
 	var editorView;
+	var workspaceElement;
+
+	function pressTab() {
+		var event = atom.keymaps.constructor.buildKeydownEvent("tab", {target: editorView});
+		atom.keymaps.handleKeyboardEvent(event);
+	}
+
+	function testSelector(selector, expected, expectedCurPos) {
+		editor.setText(selector);
+		editor.moveToEndOfLine();
+
+		atom.commands.dispatch(editorView, 'selector-to-tag:solve-selector');
+
+		expect(editor.getText()).toBe(expected);
+
+		if (expectedCurPos) {
+			expect(editor.getCursorScreenPosition()).toEqual(expectedCurPos);
+		}
+	}
 
 	beforeEach(function () {
 
 		waitsForPromise(function () {
 			return atom.workspace.open('test.html');
-		});
-
-		waitsForPromise(function () {
-			return atom.packages.activatePackage("snippets");
-		});
-
-		waitsForPromise(function () {
-			return atom.packages.activatePackage("language-html", {sync: true});
 		});
 
 		waitsForPromise(function () {
@@ -56,73 +42,80 @@ describe("Tag solver2", function () {
 		});
 	});
 
+	describe("HTML detector", function () {
+		var solver;
 
-	it("should solve block tags", function () {
+		beforeEach(function () {
+			solver = new SelectorSolver();
+		});
 
-		editor.setText("div");
-		editor.moveToEndOfLine();
-		console.log();
-		pressTab(editorView);
+		it("should handle files with no extension", function () {
+			expect(solver.isHTML("untitled")).toBe(false);
+		});
 
-		expect(editor.getText()).toBe("<div></div>");
+		it("should detect HTML files", function () {
+			expect(solver.isHTML("test.HTML")).toBe(true);
+		});
 	});
 
-});
+	describe("Tag solver", function () {
 
-describe("Tag solver", function () {
-	var solver;
+		beforeEach(function () {
+			atom.config.unset('selector-to-tag.closeSelfclosingTags');
+			atom.config.unset('selector-to-tag.expandBlockTags');
+		});
 
-	beforeEach(function () {
-		solver = new SelectorSolver();
-	});
+		it("should solve block tags", function () {
+			testSelector("div", '<div></div>', [0, 5]);
+		});
 
-	it("should solve tag alone", function () {
-		expect(solver.solveSelector("div", true).string).toBe('<div></div>');
-	});
+		it("should solve inline tags", function () {
+			testSelector("link", '<link>', [0, 5]);
+		});
 
-	it("should solve unknown tags", function () {
-		expect(solver.solveSelector("ceva", true).string).toBe('<ceva></ceva>');
-	});
+		it("should solve unknown tags", function () {
+			testSelector("ceva", '<ceva></ceva>', [0, 6]);
+		});
 
-	it("should solve tag with id", function () {
-		expect(solver.solveSelector("div#mama").string).toBe('<div id="mama"></div>');
-	});
+		it("should solve tag with id", function () {
+			testSelector("div#mama", '<div id="mama"></div>', [0, 15]);
+		});
 
-	it("should solve tag with id and class", function () {
-		expect(solver.solveSelector("div#mama.tata").string).toBe('<div id="mama" class="tata"></div>');
-	});
+		it("should solve tag with id and class", function () {
+			testSelector("div#mama.tata", '<div id="mama" class="tata"></div>', [0, 28]);
+		});
 
-	it("should solve tag with id and multiple classes", function () {
-		expect(solver.solveSelector("div#mama.tata.sora").string).toBe('<div id="mama" class="tata sora"></div>');
-	});
+		it("should solve tag with id and multiple classes", function () {
+			testSelector("div#mama.tata.sora", '<div id="mama" class="tata sora"></div>', [0, 33]);
+		});
 
-	it("should solve selectors containing - and _", function () {
-		expect(solver.solveSelector("some-tag_1#id-1_2.class_1.class-2").string).toBe('<some-tag_1 id="id-1_2" class="class_1 class-2"></some-tag_1>');
-	});
+		it("should solve selectors containing - and _", function () {
+			testSelector("some-tag_1#id-1_2.class_1.class-2", '<some-tag_1 id="id-1_2" class="class_1 class-2"></some-tag_1>', [0, 48]);
+		});
 
-	it("should solve self-closing tags", function () {
-		expect(solver.solveSelector("link", true).string).toBe('<link>');
-	});
+		it("should solve self-closing tags", function () {
+			atom.config.set('selector-to-tag.closeSelfclosingTags', true);
+			testSelector("link", '<link/>', [0, 5]);
+		});
 
-	it("should close self-closing tags", function () {
-		expect(solver.solveSelector("link", true, true).string).toBe('<link/>');
-	});
+		it("should expand block tags to multiple lines", function () {
+			atom.config.set('selector-to-tag.expandBlockTags', true);
+			testSelector("div#mama", '<div id="mama">\n\n</div>', [1, 0]);
+		});
 
-	it("should expand block tags to multiple lines", function () {
-		expect(solver.solveSelector("div#mama", true, true, true, ['div']).string).toBe('<div id="mama">\n\n</div>');
-	});
+		it("shouldn't expand tag if class or id is not specified", function () {
+			testSelector("div#", 'div#');
+			testSelector("div.", 'div.');
+		});
 
-	it("shouldn't expand tag if class or id is not specified", function () {
-		expect(solver.solveSelector("div#")).toBeNull();
-		expect(solver.solveSelector("div.")).toBeNull();
-	});
+		it("should solve to div if only class or id specified", function () {
+			testSelector("#mama", '<div id="mama"></div>', [0, 15]);
+			testSelector(".tata", '<div class="tata"></div>', [0, 18]);
+		});
 
-	it("should solve to div if only class or id specified", function () {
-		expect(solver.solveSelector(".mama").string).toBe('<div class="mama"></div>');
-		expect(solver.solveSelector("#tata").string).toBe('<div id="tata"></div>');
-	});
+		it("should not solve to div if nothing is specified", function () {
+			testSelector("", '');
+		});
 
-	it("should not solve to div if nothing is specified", function () {
-		expect(solver.solveSelector("", true)).toBeNull();
 	});
 });
